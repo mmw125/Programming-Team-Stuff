9718029dfbd7a414181640a57601ffeee251d8a7
Mark Wiggans <mmw125@gmail.com>
2017-01-30 15:52:14 -0500

    Check output when running tests

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import tempfile
 
 
 def run_command(command):
@@ -9,14 +8,26 @@ def run_command(command):
     command = '{} > {}'.format(command, 'temp')
     p = os.system(command)
     with open('temp') as temp:
-        print(temp.read())
+        out_data = temp.read()
     os.remove('temp')
+    return out_data
 
 
-def run_tests(date, program):
+def run_tests(date, program=None):
     if not os.path.exists(date):
         print("date does not exist")
         return 0
+    if program is None:
+        ran = False
+        for f in os.listdir(date):
+            if '.py' in f and 'pr' in f:
+                print(f)
+                run_tests(date, f.replace('.py', ''))
+                ran = True
+        if not ran:
+            print('No files exist')
+            return 0
+        return 1
     program_path = os.path.join(date, program) + '.py'
     if not os.path.exists(program_path):
         print("program does not exist")
@@ -30,14 +41,37 @@ def run_tests(date, program):
             files.append(file_path)
     for file in files:
         out = run_command('python {} < {}'.format(program_path, file))
+        with open(file.replace('in', 'out')) as expected_out:
+            e_out_split = expected_out.read().split('\n')
+            if out == 'unimplemented\n':
+                print('unimplemented')
+                return 0
+            out_split = out.split('\n')
+            out_split = [split for split in out_split if split]
+            for i in range(len(out_split)):
+                if i >= len(e_out_split):
+                    print(file, 'extra output', out_split[i])
+                    return 0
+                if e_out_split[i] != out_split[i]:
+                    print(file, 'expected', e_out_split[i], 'got', out_split[i])
+                    return 0
+            if len(e_out_split) > len(out_split):
+                print(file, 'not enough output')
+                return 0
+            print('passed {}'.format(file))
     return 1
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    output = None
+    if len(sys.argv) == 3:
+        output = run_tests(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 2:
+        output = run_tests(sys.argv[1])
+    else:
         print("This must be called with exactly 3 arguments")
         exit(-1)
-    if run_tests(sys.argv[1], sys.argv[2]):
+    if output:
         print("pass")
     else:
         print("failed")
